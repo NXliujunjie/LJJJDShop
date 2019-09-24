3efa9871c48ad1b87235806f4d9ba69e2e2a0043
liujunjie <18500361602@163.com>
2019-09-24 19:00:26 +0800

    提交代码

--- jdshop/lib/pages/tabs/Category/Product/ProductList.dart
+++ jdshop/lib/pages/tabs/Category/Product/ProductList.dart
@@ -8,50 +8,112 @@ class ProductList extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductList> {
+
+final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     ljjAdaper.init(context);
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('商品列表'),
+        actions: <Widget>[
+          Text(''),//去除右侧筛选按钮
+        ],
+      ),
+      endDrawer: Drawer(
+        child: Container(
+          child: Text('筛选功能'),
+        ),
       ),
       body: Stack(
         children: <Widget>[
-          _productWidget(),// 列表界面
-          Positioned(
-            top: 0,
-            height: ljjAdaper.height(80),
-            width: ljjAdaper.getScreenWidthDP(),
-            child: Container(
-              color: Colors.green,
-              height: ljjAdaper.height(80),
-              width: ljjAdaper.getScreenWidthDP(),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: InkWell(
-                      child: Text('综合'),
-                    ),
+          _productWidget(), // 列表界面
+          _headerWidget(),
+        ],
+      ),
+    );
+  }
+
+/*
+ * 筛选导航
+ */
+  Widget _headerWidget() {
+    return Positioned(
+      top: 0,
+      height: ljjAdaper.height(80),
+      width: ljjAdaper.getScreenWidthDP(),
+      child: Container(
+        height: ljjAdaper.height(80),
+        width: ljjAdaper.getScreenWidthDP(),
+        decoration: BoxDecoration(
+            border: Border(
+                bottom: BorderSide(
+                    width: 1, color: Color.fromRGBO(233, 233, 233, 0.9)))),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      0, ljjAdaper.height(20), 0, ljjAdaper.height(20)),
+                  child: Text(
+                    '综合',
+                    textAlign: TextAlign.center,
                   ),
-                  Expanded(
-                    flex: 1,
-                    child: InkWell(
-                      child: Text('销量'),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: InkWell(
-                      child: Text('综合'),
-                    ),
-                  )
-                ],
+                ),
+                onTap: () {},
               ),
             ),
-          ),
-        ],
-      )
+            Expanded(
+              flex: 1,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      0, ljjAdaper.height(20), 0, ljjAdaper.height(20)),
+                  child: Text(
+                    '销量',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      0, ljjAdaper.height(20), 0, ljjAdaper.height(20)),
+                  child: Text(
+                    '价格',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      0, ljjAdaper.height(20), 0, ljjAdaper.height(20)),
+                  child: Text(
+                    '筛选',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                onTap: () {
+                  _scaffoldKey.currentState.openEndDrawer();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -60,7 +122,7 @@ class _ProductListState extends State<ProductList> {
  */
   Widget _productWidget() {
     return Container(
-      margin: EdgeInsets.only(top:ljjAdaper.width(80)),
+      margin: EdgeInsets.only(top: ljjAdaper.width(80)),
       padding: EdgeInsets.all(ljjAdaper.width(10)),
       child: ListView.builder(
         itemBuilder: (c, i) {
